e01622a44982a921d7d83e0d60830c9d5779dd96
Juan <abrecan.eyndrill@gmail.com>
2018-10-14 11:42:06 -0400

    minor bugfixes on exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import User from './bungie/models/user';
 import Group from './bungie/models/group';
 import API from './api/bungieApi';
-import Destiny2Profile from './bungie/models/player';
+import Player from './bungie/models/player';
 
 module.exports = {
     User:  User,
     Group: Group,
     API:   API,
-    DestinyProfile: Destiny2Profile
+    Player: Player
 };
